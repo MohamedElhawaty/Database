cbfd8a889cd24e25ab748714c2ed9bdff86ed0ea
MohamedAhmed <mohamed1195ahmed@gmail.com>
2017-05-05 16:12:39 +0300

    connection

--- src/MysqlCon.java
+++ src/MysqlCon.java
@@ -6,11 +6,11 @@ class MysqlCon {
       Class.forName("com.mysql.jdbc.Driver");
       Connection con = DriverManager.getConnection(
           "jdbc:mysql://localhost:3306/bookstore", "root", "root");
-      // here sonoo is database name, root is username and password
       Statement stmt = con.createStatement();
       ResultSet rs = stmt.executeQuery("select * from publisher");
       while (rs.next())
         System.out.println(rs.getString(1) + "  " );
+      
       con.close();
     } catch (Exception e) {
       System.out.println(e);
